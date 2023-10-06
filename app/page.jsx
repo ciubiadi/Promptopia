40990de864362b6d1632899bfc9cfac69c10ac29
Adrian Albert Ciubucciu <aciubucciu@deloittece.com>
2023-10-06 23:18:21 +0300

    Finish Header Section of the Home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,18 @@
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        {/* hide it on large devices but break the content on smaller devices */}
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative propmpts
+      </p>
+
+      {/* Feed */}
+    </section>
   )
 }
 
